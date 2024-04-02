704243587976594e41b4c2270b6052075b2c4592
jmmjml <116905479+jmmjml@users.noreply.github.com>
2024-04-02 12:55:53 +0000

    first commit: feat: fonte

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  // Carregue a fonte
+  fontsLoaded = useFonts({
+    'Mulish': { uri: 'https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap' },
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontFamily: "Mulish",
+    fontWeight:"400",
+  }
 });
