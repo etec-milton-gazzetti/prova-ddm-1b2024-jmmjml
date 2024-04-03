942affafb364384a5555df184c7272b4a91136ed
jmmjml <116905479+jmmjml@users.noreply.github.com>
2024-04-03 02:39:20 +0000

    feat: Navegação entre telas

--- App.js
+++ App.js
@@ -1,26 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  // Carregue a fonte
-  fontsLoaded = useFonts({
+SplashScreen.preventAutoHideAsync();
+function HomeScreen({navigation}){
+  const [fontsLoaded, fontError] = useFonts({
     'Mulish': { uri: 'https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap' },
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
   }
-  return (
-    <View style={styles.container}>
+
+    return (
+      <View style={styles.container}>
       <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Trigonometria</Text>
+      <TouchableOpacity onPress={()=>navigation.navigate('Home')}>
+        <Text>Conteúdo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      </View>
+    );
+}
+
+  const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -33,5 +60,5 @@ const styles = StyleSheet.create({
   texto:{
     fontFamily: "Mulish",
     fontWeight:"400",
-  }
+  },
 });
